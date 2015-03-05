541974299812564bc2486aef906330be088510b6
newmanne <newman.n8@gmail.com>
2015-03-04 17:09:38 -0800

    init commit

---
+++ desktop/src/org/cpsc538B/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.cpsc538B.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.cpsc538B.TronP2PGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TronP2PGame(), config);
+	}
+}
