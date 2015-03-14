b3ac6cf859e2673fa6553375191b26435699e6d4
Sam Creed <samcreed@live.com>
2015-03-14 15:04:25 -0700

    updated server code

--- go/server.go
+++ go/server.go
@@ -1,37 +1,43 @@
-package main
-
-import "net"
-import "fmt"
-import "log"
-import "time"
+import (
+	"fmt"
+	"net"
+	"os"
+	"os/exec"
+	"strings"
+)
 
 func main() {
+	addr, err := net.ResolveUDPAddr("udp", ":11235")
+	checkError(err)
 
-	addr := net.UDPAddr{
-		Port: 1200,
-		IP: net.ParseIP("127.0.0.1"),
-	}
+	conn, err := net.ListenUDP("udp", addr)
+	checkError(err)
 
-	conn, err := net.ListenUDP("udp", &addr)
-	if err != nil {
-		fmt.Println("here 0")
-		log.Fatal(err)
-	}
-	defer conn.Close()
-
-	var buf []byte = make([]byte, 1500)
+	buf := make([]byte, 1024)
 	for {
-		time.Sleep(100 * time.Millisecond)
-		n, addr, err := conn.ReadFromUDP(buf)
-		fmt.Println("msg: ", string(buf[0:n]))
+		_, raddr, err := conn.ReadFromUDP(buf)
+		checkError(err)
+		fmt.Fprintf(os.Stdout, "Received packet from: %s\n", raddr)
+		buf = []byte(getFortune())
+		n, err := conn.WriteToUDP(buf, raddr)
+		fmt.Fprintf(os.Stdout, "Wrote fortune of %d bytes\n", n)
+	}
+}
 
-		if err != nil {
-			log.Fatal(err)
-		}
+// Returns random quote of the day using the build-in fortune
+// program. If this fails, then it returns a hardcoded quote.
+func getFortune() string {
+	out, err := exec.Command("fortune", "-s").Output()
+	if err != nil {
+		return "The goal of Computer Science is to build something that will last at least until we've finished building it."
+	}
+	return strings.TrimSpace(string(out))
+}
 
-		n, err = conn.WriteToUDP(buf, addr)
-		if err != nil {
-			log.Fatal(err)
-		}
+// If error is non-nil, print it out and halt.
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error ", err.Error())
+		os.Exit(1)
 	}
 }
