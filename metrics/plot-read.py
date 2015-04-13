f1973f432860ce5c35ca3d0a3eb6df899c7da1db
Sam Creed <samcreed92@gmail.com>
2015-04-13 15:00:39 -0700

    remove debug statements from go

--- metrics/plot-read.py
+++ metrics/plot-read.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import csv
 
 # calculate avg read per round (for leader and clients)
-playerSizes = [1, 2, 3, 4, 5, 6, 7, 8, 17]
+playerSizes = [1, 2, 3, 4, 5, 6, 7, 8]
 
 readAvgsLeader = []
 readAvgsClient1 = []
@@ -32,5 +32,34 @@ for i in playerSizes:
 		readAvgsClient1.append(readSumClient1 / lastRound)
 		#readAvgsClient2.append(readSumClient2 / lastRound)
 
-plt.plot(playerSizes, readAvgsLeader, playerSizes, readAvgsClient1, marker='o')
+readAvgsClient1[0] = readAvgsLeader[0]
+
+# read throughput
+plt.scatter(playerSizes, readAvgsLeader, color='r')
+plt.scatter(playerSizes, readAvgsClient1, color='b')
+
+# determine best fit line (leader)
+par = np.polyfit(playerSizes, readAvgsLeader, 1, full=True)
+slope=par[0][0]
+intercept=par[0][1]
+xl = [min(playerSizes), max(playerSizes)]
+yl = [slope*xx + intercept for xx in xl]
+plt.plot(xl, yl, '-r', label='leader')
+
+# determine best fit line (client)
+par = np.polyfit(playerSizes, readAvgsClient1, 1, full=True)
+slope=par[0][0]
+intercept=par[0][1]
+x2 = [min(playerSizes), max(playerSizes)]
+y2 = [slope*xx + intercept for xx in x2]
+plt.plot(x2, y2, '--b', label ='client')
+
+plt.title('Read Throughput by # Players')
+plt.xlabel("# Players")
+plt.ylabel("Average # Bytes Read per Round")
+plt.legend(loc='upper left')
+
 plt.show()
+
+# write throughput (TODO, for project report)
+
